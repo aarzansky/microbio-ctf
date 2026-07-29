e8642764d3bc48ae2b832a99a4283fdd7ab772db
aarzansky <shakyaaarzan@gmail.com>
2026-07-29 07:40:29 +0545

    Updated Questions

--- backend/utils/db-extra.sql
+++ backend/utils/db-extra.sql
@@ -16,4 +16,11 @@ user_id	    int(11)	NO	    PRI
 question_id	int(11)	NO	    PRI	
 attempts	int(11)	NO		            0
 
+create table user_attempts (
+    user_id INT NOT NULL,
+    question_id INT NOT NULL,
+    attempts INT DEFAULT 0,
+    PRIMARY KEY (user_id, question_id)
+);
+
 INSERT INTO score (username, user_score) VALUES ('user3', JSON_ARRAY());
